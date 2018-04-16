dbf8b6d9367f16fac34f6a8260b78ec159380bdf
Thanh Binh <binhhoit@gmail.com>
2018-04-16 16:11:48 +0700

    Initial Commit

---
+++ billby/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  billby
+//
+//  Created by Macbook Air on 4/16/18.
+//  Copyright © 2018 BeesightSoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
